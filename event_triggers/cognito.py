964b38681e944ec27ee23d4f0d7547740b3747cc
Barry Liu <barry.y.liu@hotmail.com>
2022-02-23 11:10:50 +0800

    [U] Update

--- event_triggers/cognito.py
+++ event_triggers/cognito.py
@@ -44,132 +44,146 @@ class Cognito(object):
 
     # Trigger of pre-generate token
     def pre_token_generate(self, event, context):
-        database_type = self.setting.get("type", "mysql")
-        driver_name = self.setting.get("driver", "pymysql")
-        charset = self.setting.get("charset", "utf8mb4")
-        sources = [
-            "tokengeneration_hostedauth",
-            "tokengeneration_newpasswordchallenge",
-            "tokengeneration_authenticatedevice",
-            "tokengeneration_refreshtokens",
-            "tokengeneration_authentication",
-        ]
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Pre token generate >>>>>>>>>>>>")
+        try:
+            database_type = self.setting.get("type", "mysql")
+            driver_name = self.setting.get("driver", "pymysql")
+            charset = self.setting.get("charset", "utf8mb4")
+            sources = [
+                "tokengeneration_hostedauth",
+                "tokengeneration_newpasswordchallenge",
+                "tokengeneration_authenticatedevice",
+                "tokengeneration_refreshtokens",
+                "tokengeneration_authentication",
+            ]
 
-        if (
-            event
-            and event.get("triggerSource")
-            and sources.__contains__(str(event.get("triggerSource")).strip().lower())
-            and self.setting.get("user")
-            and self.setting.get("password")
-            and self.setting.get("host")
-            and self.setting.get("port")
-            and self.setting.get("schema")
-            and event.get("request", {}).get("userAttributes", {}).get("sub")
-        ):
-            dsn = "{}+{}://{}:{}@{}:{}/{}?charset={}".format(
-                database_type,
-                driver_name,
-                self.setting.get("user"),
-                self.setting.get("password"),
-                self.setting.get("host"),
-                self.setting.get("port"),
-                self.setting.get("schema"),
-                charset,
-            )
-            engine = create_engine(dsn)
-            session = scoped_session(
-                sessionmaker(autocommit=False, autoflush=False, bind=engine)
-            )
-            # 1. Get user
-            cognito_user_id = (
-                event.get("request", {}).get("userAttributes", {}).get("sub")
-            )
-            user = (
-                session.query(UserModel)
-                .filter_by(cognito_user_sub=cognito_user_id)
-                .first()
-            )
-            claimsToAddOrOverride = {
-                "source": str(user.source if user.source else UserSource.SS3.value)
-            }
-
-            if user:
-                # 1. Attach user id to token.
-                if user.id is not None:
-                    claimsToAddOrOverride["user_id"] = str(user.id)
-
-                # 2. Is admin user
-                if user.is_admin is not None:
-                    claimsToAddOrOverride["is_admin"] = str(user.is_admin)
-
-                # 3. Get seller / teams info
-                if user.seller_id and not user.is_admin:
-                    claimsToAddOrOverride["seller_id"] = str(user.seller_id)
-                    # 3.1. Get seller info
-                    seller = (
-                        session.query(SellerModel)
-                        .order_by(SellerModel.seller_name.asc())
-                        .filter_by(seller_id=user.seller_id)
-                    ).first()
-
-                    if seller.s_vendor_id:
-                        claimsToAddOrOverride["s_vendor_id"] = str(seller.s_vendor_id)
-
-                    # 3.2. Get teams by seller id
-                    permission_filter_conditions = (
-                        RelationshipModel.user_id == str(cognito_user_id).strip()
-                    ) & (
-                        RelationshipModel.type.is_in(
-                            RoleRelationshipType.ADMINISTRATOR.value,
-                            RoleRelationshipType.COMPANY.value,
-                        )
-                    )
-                    team_filter_conditions = [TeamModel.seller_id == user.seller_id]
-
-                    if user.id is not None:
-                        permission_filter_conditions = (
-                            permission_filter_conditions
-                        ) | (RelationshipModel.user_id == str(user.id).strip())
-
-                    team_ids = list(
-                        set(
-                            [
-                                relationship.group_id
-                                for relationship in RelationshipModel.scan(
-                                    filter_condition=permission_filter_conditions
-                                )
-                                if not relationship.group_id
-                            ]
-                        )
-                    )
-
-                    if len(team_ids):
-                        team_filter_conditions.append(TeamModel.team_id.in_(team_ids))
-
-                    seller_teams = (
-                        session.query(TeamModel).filter(*team_filter_conditions).all()
-                    )
-                    teams = {}
-
-                    for team in seller_teams:
-                        if team and team.team_id:
-                            item = {
-                                "team_id": team.team_id,
-                                "type": team.type,
-                                "vendor_id": team.vendor_id,
-                                "erp_vendor_ref": team.erp_vendor_ref,
-                            }
-
-                            teams.update({team.team_id: item})
-
-                    claimsToAddOrOverride.update({"teams": json.dumps(teams)})
-
-            if len(claimsToAddOrOverride.keys()):
-                event["response"]["claimsOverrideDetails"] = {
-                    "claimsToAddOrOverride": claimsToAddOrOverride
+            if (
+                event
+                and event.get("triggerSource")
+                and sources.__contains__(
+                    str(event.get("triggerSource")).strip().lower()
+                )
+                and self.setting.get("user")
+                and self.setting.get("password")
+                and self.setting.get("host")
+                and self.setting.get("port")
+                and self.setting.get("schema")
+                and event.get("request", {}).get("userAttributes", {}).get("sub")
+            ):
+                dsn = "{}+{}://{}:{}@{}:{}/{}?charset={}".format(
+                    database_type,
+                    driver_name,
+                    self.setting.get("user"),
+                    self.setting.get("password"),
+                    self.setting.get("host"),
+                    self.setting.get("port"),
+                    self.setting.get("schema"),
+                    charset,
+                )
+                engine = create_engine(dsn)
+                session = scoped_session(
+                    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+                )
+                # 1. Get user
+                cognito_user_id = (
+                    event.get("request", {}).get("userAttributes", {}).get("sub")
+                )
+                user = (
+                    session.query(UserModel)
+                    .filter_by(cognito_user_sub=cognito_user_id)
+                    .first()
+                )
+                claimsToAddOrOverride = {
+                    "source": str(user.source if user.source else UserSource.SS3.value)
                 }
 
-            session.close()
+                if user:
+                    # 1. Attach user id to token.
+                    if user.id is not None:
+                        claimsToAddOrOverride["user_id"] = str(user.id)
 
-        # Return to Amazon Cognito
-        return event
+                    # 2. Is admin user
+                    if user.is_admin is not None:
+                        claimsToAddOrOverride["is_admin"] = str(user.is_admin)
+
+                    # 3. Get seller / teams info
+                    if user.seller_id and not user.is_admin:
+                        claimsToAddOrOverride["seller_id"] = str(user.seller_id)
+                        # 3.1. Get seller info
+                        seller = (
+                            session.query(SellerModel)
+                            .order_by(SellerModel.seller_name.asc())
+                            .filter_by(seller_id=user.seller_id)
+                        ).first()
+
+                        if seller.s_vendor_id:
+                            claimsToAddOrOverride["s_vendor_id"] = str(
+                                seller.s_vendor_id
+                            )
+
+                        # 3.2. Get teams by seller id
+                        permission_filter_conditions = (
+                            RelationshipModel.user_id == str(cognito_user_id).strip()
+                        ) & (
+                            RelationshipModel.type.is_in(
+                                RoleRelationshipType.ADMINISTRATOR.value,
+                                RoleRelationshipType.COMPANY.value,
+                            )
+                        )
+                        team_filter_conditions = [TeamModel.seller_id == user.seller_id]
+
+                        if user.id is not None:
+                            permission_filter_conditions = (
+                                permission_filter_conditions
+                            ) | (RelationshipModel.user_id == str(user.id).strip())
+
+                        team_ids = list(
+                            set(
+                                [
+                                    relationship.group_id
+                                    for relationship in RelationshipModel.scan(
+                                        filter_condition=permission_filter_conditions
+                                    )
+                                    if not relationship.group_id
+                                ]
+                            )
+                        )
+
+                        if len(team_ids):
+                            team_filter_conditions.append(
+                                TeamModel.team_id.in_(team_ids)
+                            )
+
+                        seller_teams = (
+                            session.query(TeamModel)
+                            .filter(*team_filter_conditions)
+                            .all()
+                        )
+                        teams = {}
+
+                        for team in seller_teams:
+                            if team and team.team_id:
+                                item = {
+                                    "team_id": team.team_id,
+                                    "type": team.type,
+                                    "vendor_id": team.vendor_id,
+                                    "erp_vendor_ref": team.erp_vendor_ref,
+                                }
+
+                                teams.update({team.team_id: item})
+
+                        claimsToAddOrOverride.update({"teams": json.dumps(teams)})
+
+                if len(claimsToAddOrOverride.keys()):
+                    event["response"]["claimsOverrideDetails"] = {
+                        "claimsToAddOrOverride": claimsToAddOrOverride
+                    }
+
+                session.close()
+            # Return to Amazon Cognito
+            return event
+        except Exception as e:
+            print(">>>>>> Pre token generate exception::::", e)
+            raise e
+        finally:
+            return event
